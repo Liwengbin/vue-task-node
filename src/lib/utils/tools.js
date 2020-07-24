5a5998fa26bd5b368d2bbb073d112f3424981b2f
zhiyaohaha <bo5080110@163.com>
2020-07-24 11:53:56 +0800

    解决chrome浏览器 在系统缩放时 连线不正确

--- src/lib/utils/tools.js
+++ src/lib/utils/tools.js
@@ -20,3 +20,28 @@ export function browsers () {
     return 'Firefox'
   }
 }
+// 判断缩放
+export function zoomRatio () {
+  let ratio = 0
+  let screen = window.screen
+  let ua = navigator.userAgent.toLowerCase()
+
+  if (window.devicePixelRatio !== undefined) {
+    ratio = window.devicePixelRatio
+  } else if (~ua.indexOf('msie')) {
+    if (screen.deviceXDPI && screen.logicalXDPI) {
+      ratio = screen.deviceXDPI / screen.logicalXDPI
+    }
+  } else if (window.outerWidth !== undefined && window.innerWidth !== undefined) {
+    ratio = window.outerWidth / window.innerWidth
+  }
+
+  if (ratio) {
+    ratio = Math.round(ratio * 100)
+  }
+  if (browsers() === 'Chrome') {
+    return ratio
+  } else {
+    return 100
+  }
+}
